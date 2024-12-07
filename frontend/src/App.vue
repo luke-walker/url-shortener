7d7f9c4a3527b03ad49b62f4754d55809e47c64a
Luke Walker <lukeryanwalker2000@gmail.com>
2024-12-06 22:04:16 -0800

    Initial commit - frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+    name: "App"
+});
+</script>
+
+<template>
+<div id="app">
+
+</div>
+</template>
